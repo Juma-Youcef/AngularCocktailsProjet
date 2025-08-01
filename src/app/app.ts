ee9ac34d22cf9fcbea9af751dacb252e843e63f3
Juma-Youcef <youcef.lamara.prof@gmail.com>
2025-08-01 13:31:40 +0100

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Footer } from './component/footer/footer';
+import { Header } from './component/header/header';
+import { Cocktails } from './component/cocktails/cocktails';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [Footer, Header, Cocktails],
+  template: `
+    <app-header></app-header>
+    <app-cocktails></app-cocktails>
+    <app-footer></app-footer>
+
+  `,
+  styles: [`
+    :host {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+    }
+
+    app-cocktails {
+      flex: 1;
+    }
+  `]
 })
 export class App {
   protected readonly title = signal('Project');
